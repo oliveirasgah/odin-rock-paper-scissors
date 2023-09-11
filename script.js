810f148f3eb588cea06bbc960b9a250889204039
Gabriel Oliveira dos Santos <oliveirasgah@gmail.com>
2023-09-11 19:51:23 -0300

    Remove unused function

--- script.js
+++ script.js
@@ -35,41 +35,6 @@ function playRound(plyChoice, cpuChoice) {
     return playerWon;
 }
 
-function game() {
-    let playerPoints = 0;
-    let cpuPoints = 0;
-
-    for(let i = 1; i <= 1; i++) {
-        let playerChoice = prompt('Rock, paper, or scissors?').toUpperCase();
-        let cpuChoice = getComputerChoice().toUpperCase();
-
-        let result = playRound(playerChoice, cpuChoice);
-
-        console.log(`Round ${i}:`);
-
-        if(result == null) {
-            console.log(`It's a draw! ${playerChoice} and ${cpuChoice} are equal!`)
-        } else if(result) {
-            console.log(`You win! ${playerChoice} beats ${cpuChoice}!`)
-            playerPoints++;
-        } else {
-            console.log(`You lose! ${cpuChoice} beats ${playerChoice}!`)
-            cpuPoints++;
-        }
-
-        console.log('\n');
-    }
-
-    if(playerPoints > cpuPoints)
-        console.log('Congratulations! You won the game!');
-    else if(cpuPoints > playerPoints)
-        console.log('You lost! Better luck next time!');
-    else
-        console.log('The game ended in a draw.');
-
-    console.log(`Final score: ${playerPoints}:${cpuPoints}`);
-}
-
 function updateStyles(resultRound) {
     const playerScore = document.querySelector('.player_score');
     const playerStrong = document.querySelector('.player_strong');
